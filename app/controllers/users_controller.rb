fdfbbfa7afc50cf28941177a21d689b6364772db
Josh <kyburzjz@gmail.com>
2016-03-19 21:13:10 +0000

    Added friendship table and model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,11 @@
 class UsersController < ApplicationController
+  
   def my_portfolio
     @user_stocks = current_user.stocks
     @user = current_user
+  end
+  
+  def my_friends
+    
   end
 end
